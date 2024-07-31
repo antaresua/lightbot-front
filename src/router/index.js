d3be7d79ff01a03e205db7e33082e7da072b40e6
Ihor Bondarenko <bondarenkoid.dev@gmail.com>
2024-07-31 17:46:53 +0300

    add TimeSlot editing form

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import DayForm from '../components/DayForm.vue';
 import DayEdit from '../components/DayEdit.vue';
 import TimeSlotList from '../components/TimeSlotList.vue';
 import TimeSlotForm from '../components/TimeSlotForm.vue';
+import TimeSlotEdit from '../components/TimeSlotEdit.vue';
 // Імпортуйте інші компоненти
 
 const routes = [
@@ -36,7 +37,7 @@ const routes = [
   {
     path: '/timeslots/:id/edit',
     name: 'TimeSlotEdit',
-    component: TimeSlotForm,
+    component: TimeSlotEdit,
     props: true
   }
 ];
