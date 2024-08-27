b2bab6311b3197ed9f9cc9912ee35d24fdeb0f95
Antares <antares@evoplay.tech>
2024-08-27 14:45:08 +0300

    add temp disabled dropdown menu for schedule config

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#" style="font-weight: bolder;">СвітлоБот</a>
-                <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon">
                         <span></span>
                         <span></span>
@@ -19,6 +21,16 @@
                             <router-link class="nav-link" to="/statuses">Лог подій</router-link>
                         </li>
                         <!-- Додайте інші посилання на сторінки, якщо потрібно -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle disabled" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false" aria-disabled="true">
+                                Управління графіком
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><router-link to="/days" class="dropdown-item">Дні тижня</router-link></li>
+                                <li><router-link to="/timeslots" class="dropdown-item">Тайм-слоти</router-link></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -66,14 +78,17 @@ body {
 }
 
 .navbar-toggler.custom-toggler .navbar-toggler-icon {
-    background-image: none; /* Прибирає стандартну іконку Bootstrap */
+    background-image: none;
+    /* Прибирає стандартну іконку Bootstrap */
 }
 
 .navbar-toggler.custom-toggler .navbar-toggler-icon span {
     display: block;
     width: 30px;
     height: 3px;
-    background-color: #42b983; /* Зелений колір ліній */
-    margin: 5px 0; /* Відстань між лініями */
+    background-color: #42b983;
+    /* Зелений колір ліній */
+    margin: 5px 0;
+    /* Відстань між лініями */
 }
 </style>
