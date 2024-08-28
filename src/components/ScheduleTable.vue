9a775760eaa096c1f3e0f3a5d04ac3680ddeb1cc
Antares <antares@evoplay.tech>
2024-08-28 12:30:41 +0300

    fix schedule styles

--- src/components/ScheduleTable.vue
+++ src/components/ScheduleTable.vue
@@ -9,28 +9,31 @@
                         <th v-for="hour in hours" :key="hour" class="rotate-text" :data-hour="hour"></th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr v-for="day in sortedDays" :key="day.name" @mouseover="highlightRow($event)"
-                        @mouseout="removeHighlight($event)">
+                <tbody v-if="days.length">
+                    <tr v-for="day in sortedDays" :key="day.dayOfWeek" @mouseover="highlightRow"
+                        @mouseout="removeHighlight">
                         <td colspan="2" class="day-name">{{ day.name }}</td>
                         <td v-for="hour in hours" :key="hour" :class="getClassForHour(day.dayOfWeek, hour)"
-                            @mouseover="highlightCell($event)" @mouseout="removeCellHighlight($event)"></td>
+                            @mouseover="highlightCell" @mouseout="removeCellHighlight">
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody v-else>
+                    <tr>
+                        <td colspan="2">Loading...</td>
                     </tr>
                 </tbody>
             </table>
         </div>
         <div class="legend">
             <div class="legend-item">
-                <span class="legend-color cell-off"></span>
-                - Відключення світла
+                <span class="legend-color cell-off"></span> - Відключення світла
             </div>
             <div class="legend-item">
-                <span class="legend-color cell-possible-on"></span>
-                - Можливе включення
+                <span class="legend-color cell-possible-on"></span> - Можливе включення
             </div>
             <div class="legend-item">
-                <span class="legend-color cell-on"></span>
-                - Світло є
+                <span class="legend-color cell-on"></span> - Світло є
             </div>
         </div>
         <div class="subscribe-button-container">
@@ -48,21 +51,12 @@ export default {
     data() {
         return {
             timeSlots: [],
-            days: [
-                { name: 'Неділя', dayOfWeek: 0 },
-                { name: 'Понеділок', dayOfWeek: 1 },
-                { name: 'Вівторок', dayOfWeek: 2 },
-                { name: 'Середа', dayOfWeek: 3 },
-                { name: 'Четвер', dayOfWeek: 4 },
-                { name: 'П’ятниця', dayOfWeek: 5 },
-                { name: 'Субота', dayOfWeek: 6 },
-            ],
+            days: [],
             hours: ['00-01', '01-02', '02-03', '03-04', '04-05', '05-06', '06-07', '07-08', '08-09', '09-10', '10-11', '11-12', '12-13', '13-14', '14-15', '15-16', '16-17', '17-18', '18-19', '19-20', '20-21', '21-22', '22-23', '23-24'],
         };
     },
     computed: {
         sortedDays() {
-            // Сортуємо дні так, щоб Неділя буде останньою
             const sorted = [...this.days].sort((a, b) => a.dayOfWeek - b.dayOfWeek);
             const sunday = sorted.find(day => day.dayOfWeek === 0);
             const otherDays = sorted.filter(day => day.dayOfWeek !== 0);
@@ -70,6 +64,14 @@ export default {
         }
     },
     methods: {
+        async fetchDays() {
+            try {
+                const response = await apiService.getDays();
+                this.days = response.data;
+            } catch (error) {
+                console.error('Error fetching days:', error);
+            }
+        },
         async fetchTimeSlots() {
             try {
                 const response = await apiService.getTimeSlots();
@@ -79,7 +81,7 @@ export default {
             }
         },
         isHourInTimeSlot(hour, timeSlot) {
-            const [hourStart] = hour.split('-').map(h => Number(h.split(':')[0]));
+            const [hourStart] = hour.split('-').map(Number);
             const [slotStartHour, slotStartMinute] = timeSlot.startTime.split(':').map(Number);
             const [slotEndHour, slotEndMinute] = timeSlot.endTime.split(':').map(Number);
 
@@ -127,25 +129,28 @@ export default {
             cell.classList.remove('highlight-cell');
         },
         subscribeToTelegram() {
-            window.location.href = 'https://t.me/svitlobot_em_e2a'; // Додайте посилання на ваш Telegram канал
+            window.location.href = 'https://t.me/svitlobot_em_e2a';
         },
         highlightCurrentTime() {
             const now = new Date();
-            const currentDayOfWeek = now.getDay(); // Отримуємо день тижня (0 - неділя, 1 - понеділок, ... 6 - субота)
-
-            // Додаємо 1 до індексу, щоб врахувати, що Неділя тепер в кінці
-            const dayRow = this.$el.querySelector(`tbody tr:nth-child(${(currentDayOfWeek === 0 ? 6 : currentDayOfWeek - 1) + 1})`);
+            const currentDayOfWeek = now.getDay();
+            const dayRowIndex = (currentDayOfWeek === 0 ? 6 : currentDayOfWeek - 1) + 1;
+            const dayRow = this.$el.querySelector(`tbody tr:nth-child(${dayRowIndex})`);
             if (dayRow) {
                 this.highlightRow({ currentTarget: dayRow });
             }
         }
     },
     mounted() {
-        this.fetchTimeSlots();
-        this.highlightCurrentTime();
-    },
+        this.fetchDays().then(() => {
+            this.fetchTimeSlots().then(() => {
+                this.highlightCurrentTime();
+            });
+        });
+    }
 }
 </script>
+
 <style scoped>
 .table-container {
     max-width: 1200px;
@@ -176,9 +181,9 @@ export default {
 .time-slots-table th,
 .time-slots-table td {
     border: 1px solid #ddd;
-    padding: 12px;
+    padding: 13px;
     text-align: center;
-    font-size: 14px;
+    font-size: 16px;
     min-width: 40px;
     width: 40px;
 }
@@ -245,12 +250,11 @@ export default {
     transform: translate(-50%, -50%) rotate(-90deg);
     transform-origin: center;
     white-space: nowrap;
-    font-size: 12px;
+    font-size: 14px;
     display: block;
 }
 
 .day-name {
-    font-weight: bold;
     font-size: 14px;
     white-space: nowrap;
 }
@@ -315,7 +319,7 @@ export default {
     .time-slots-table th,
     .time-slots-table td {
         border: 1px solid #ddd;
-        padding: 12px;
+        padding: 10px;
         text-align: center;
         font-size: 14px;
         min-width: 35px;
@@ -341,9 +345,9 @@ export default {
     .time-slots-table th,
     .time-slots-table td {
         border: 1px solid #ddd;
-        padding: 12px;
+        padding: 7px;
         text-align: center;
-        font-size: 14px;
+        font-size: 12px;
         min-width: 25px;
         width: 25px;
     }
